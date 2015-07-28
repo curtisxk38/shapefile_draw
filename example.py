115566c5c711129a2659ba421d9e0efbde2aa729
curtisxk38 <curtisxk38@scribe.33mail.com>
2015-07-27 22:26:38 -0400

    Fixed source of major slowdown It was caused by my_modify function in the example, not by the actual program

--- example.py
+++ example.py
@@ -2,8 +2,8 @@ from PIL import Image
 import shapefile_draw
 
 def my_modify(self, new_point):
-	new_point[0] -= self.sh_list[0].shapes()[0].bbox[0]
-	new_point[1] -= self.sh_list[0].shapes()[0].bbox[1]
+	new_point[0] -= self.bbox[0]
+	new_point[1] -= self.bbox[1]
 	new_point[0] //= 8
 	new_point[1] //= 8
 	
@@ -11,7 +11,10 @@ def my_modify(self, new_point):
 im = Image.new('RGBA', (2786, 2770), (255, 255, 255, 0))
 pic = shapefile_draw.ShapefileDrawer(im, modify_function=my_modify)
 pic.load_shapefile("city_limit/municipal_boundary_area_06_25_2015", "black")
-#pic.load_shapefile("road_area/road_area_06_30_2015", "black")
+pic.load_shapefile("road_area/road_area_06_30_2015", "black")
 pic.load_shapefile("bicycle_lane/bicycle_lane_line_06_27_2015", "blue")
+
+# this is only needed for my example, because of my_modify
+pic.bbox = pic.sh_list[0].shapes()[0].bbox
 
 pic.draw()
